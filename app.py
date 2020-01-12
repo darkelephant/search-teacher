e0ccb3488a8fda1da7b3a46c5063e3524931e49b
Dark Elephant <holopov@yandex.ru>
2020-01-12 19:34:40 +0300

    Убрана переменная page, сразу возвращается render_template

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import json
 import random
 from flask import Flask, render_template, request
 
-app = Flask(__name__)
-
 def load_json(file_name):
     with open(file_name, 'r', encoding='utf8') as f:
         contents = f.read()
@@ -17,14 +15,15 @@ def save_json(file_name, data_json):
 list_goals = load_json('goals.json')
 teachers = load_json('teachers.json')
 
+app = Flask(__name__)
+
 @app.route('/')
 @app.route('/index')
 def index():
     all_teachers = [x for x in teachers]
     random.seed()
     random.shuffle(all_teachers)
-    page = render_template("index.html", profiles = teachers, six_teachers = all_teachers[:6],  goals = list_goals)
-    return page
+    return render_template("index.html", profiles = teachers, six_teachers = all_teachers[:6],  goals = list_goals)
 
 @app.route('/goals/<goal>')
 def goals(goal):
@@ -33,21 +32,18 @@ def goals(goal):
         if goal in profile_teach['goals']:
             list_id_teachers.append(key_teach)
     print(list_id_teachers)
-    page = render_template("goal.html", profiles = teachers, teachers = list_id_teachers, goal = list_goals[goal])
-    return page
+    return render_template("goal.html", profiles = teachers, teachers = list_id_teachers, goal = list_goals[goal])
 
 @app.route('/profiles/<id_teach>')
 def profile(id_teach):
     print(id_teach)
     try:
         if int(id_teach) > 0:
-            page = render_template("profile.html", profile = teachers[id_teach], goals = list_goals, id = id_teach)
-            return page
+            return render_template("profile.html", profile = teachers[id_teach], goals = list_goals, id = id_teach)
         else:
-            page = render_template("profiles.html", profiles = teachers)
-            return page
+            return render_template("profiles.html", profiles = teachers)
     except:
-        return '<p>ОШИБКА</p>'
+        return '<p>Ошибка значения идентификатора</p>'
 
 @app.route('/search', methods=['GET'])
 def search():
@@ -55,8 +51,7 @@ def search():
     
 @app.route('/request')
 def selection():
-    page = render_template("pick.html", goals = list_goals)
-    return page
+    return render_template("pick.html", goals = list_goals)
     
 @app.route('/send_request', methods=['POST'])
 def send_selection():
@@ -72,8 +67,7 @@ def send_selection():
     else:
         request_json = {'0':{'goal':goal, 'time':time, 'fio':fio, 'phone':phone}}
         save_json('request.json', request_json)
-    page = render_template("sent_pick.html", fio = fio, phone = phone)
-    return page
+    return render_template("sent_pick.html", fio = fio, phone = phone)
 
 @app.route('/booking/<id_teach>', methods=['GET'])
 def booking(id_teach):
@@ -82,8 +76,7 @@ def booking(id_teach):
 @app.route('/message', methods=['GET'])
 def message():
     id_teach = request.args['id']
-    page = render_template("message.html", profile = teachers[id_teach], id = id_teach)
-    return page
+    return render_template("message.html", profile = teachers[id_teach], id = id_teach)
 
 @app.route('/send_message', methods=['POST'])
 def send_message():
@@ -91,8 +84,7 @@ def send_message():
     fio = request.form.get('fio')
     phone = request.form.get('phone')
     text_message = request.form.get('text_message')
-    page = render_template("sent_message.html", profile = teachers[id_teach], fio = fio, phone = phone, text_message = text_message)
-    return page
+    return render_template("sent_message.html", profile = teachers[id_teach], fio = fio, phone = phone, text_message = text_message)
 
 if __name__ == "__main__":
     app.debug = True
